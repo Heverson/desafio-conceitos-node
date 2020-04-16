f6fa0edbea4e875fbaf30455212f402c5f4c722a
Heverson Damasceno <heversondamasceno@gmail.com>
2020-04-16 07:05:29 -0300

    Add funcionalidades da Api

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,66 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+  const repositorie = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes:0
+  };
+  repositories.push(repositorie);
+  return response.status(201).json(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title, url, techs} = request.body;
+  
+  const repositorieIndex = repositories.findIndex(repo => repo.id === id);
+  if(repositorieIndex < 0){
+    return response.status(400).json({error:"Repositorie NOT FOUND"});
+  }
+  const repositorie = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositorieIndex].likes
+  }
+  repositories[repositorieIndex] = repositorie; 
+  return response.json(repositories[repositorieIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  
+  const repositorieIndex = repositories.findIndex(repo => repo.id === id);
+  if(repositorieIndex < 0){
+    return response.status(400).json({error:"Repositorie NOT FOUND"});
+  }
+
+  repositories.splice(repositorieIndex,1);
+  return response.status(204).send();  
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const repositorieIndex = repositories.findIndex(repo => repo.id === id);
+  if(repositorieIndex < 0){
+    return response.status(400).json({error:"Repositorie NOT FOUND"});
+  }
+  const repositorie = {...repositories[repositorieIndex],
+    likes: repositories[repositorieIndex].likes+1
+  }
+  repositories[repositorieIndex] = repositorie; 
+  return response.json(repositories[repositorieIndex]);
 });
 
 module.exports = app;
